d2f70a01c5dc5c0b42409c3dc3d2c615f3f898c6
eccs0103 <eccs0103@gmail.com>
2023-06-19 02:59:26 +0400

    1.2.8

--- scripts/modules/database.js
+++ scripts/modules/database.js
@@ -4,44 +4,32 @@
 * @template Notation
 */
 class Database {
-	static #locked = true;
 	/**
-	 * @template Notation
 	 * @param {String} path 
 	 */
-	static async construct(path) {
-		Database.#locked = false;
-		/** @type {Database<Notation>} */ const instance = new Database();
-		Database.#locked = true;
-		//
-		instance.#store = await (/** @type {Promise<IDBObjectStore>} */ (new Promise((resolve, reject) => {
-			const request = indexedDB.open(`database`, 1);
-			request.addEventListener(`upgradeneeded`, (event) => {
-				instance.#store = request.result.createObjectStore(path);
-			});
-			request.addEventListener(`success`, (event) => {
-				const transaction = request.result.transaction(path, `readwrite`);
-				resolve(transaction.objectStore(path));
-			});
-			request.addEventListener(`error`, (event) => {
-				reject(request.error);
-			});
-		})));
-		//
-		return instance;
+	constructor(path) {
+		this.#path = path;
 	}
-	constructor() {
-		if (Database.#locked) {
-			throw new TypeError(`Illegal constructor`);
-		}
-	}
-	/** @type {IDBObjectStore} */ #store;
+	/** @type {String} */ #path;
 	/**
 	 * @param {String} key 
 	 * @returns {Promise<Notation>} 
 	 */
 	async get(key) {
-		/** @type {IDBRequest<Notation>} */ const request = this.#store.get(key);
+		const store = await (/** @type {Promise<IDBObjectStore>} */ (new Promise((resolve, reject) => {
+			const request = indexedDB.open(`database`, 1);
+			request.addEventListener(`upgradeneeded`, (event) => {
+				request.result.createObjectStore(this.#path);
+			});
+			request.addEventListener(`success`, (event) => {
+				const transaction = request.result.transaction(this.#path, `readwrite`);
+				resolve(transaction.objectStore(this.#path));
+			});
+			request.addEventListener(`error`, (event) => {
+				reject(request.error);
+			});
+		})));
+		/** @type {IDBRequest<Notation>} */ const request = store.get(key);
 		return new Promise((resolve, reject) => {
 			request.addEventListener(`success`, (event) => {
 				resolve(request.result);
@@ -57,7 +45,20 @@ class Database {
 	 * @returns {Promise<void>} 
 	 */
 	async set(key, value) {
-		/** @type {IDBRequest<IDBValidKey>} */ const request = this.#store.put(value, key);
+		const store = await (/** @type {Promise<IDBObjectStore>} */ (new Promise((resolve, reject) => {
+			const request = indexedDB.open(`database`, 1);
+			request.addEventListener(`upgradeneeded`, (event) => {
+				request.result.createObjectStore(this.#path);
+			});
+			request.addEventListener(`success`, (event) => {
+				const transaction = request.result.transaction(this.#path, `readwrite`);
+				resolve(transaction.objectStore(this.#path));
+			});
+			request.addEventListener(`error`, (event) => {
+				reject(request.error);
+			});
+		})));
+		/** @type {IDBRequest<IDBValidKey>} */ const request = store.put(value, key);
 		return new Promise((resolve, reject) => {
 			request.addEventListener(`success`, (event) => {
 				resolve();
