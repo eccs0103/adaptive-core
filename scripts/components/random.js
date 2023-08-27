3b6aa01f625a0c15d311989ccc8a6efa2b906a85
Arman <eccs0103@gmail.com>
2023-08-27 19:30:33 +0400

    2.1.0

--- scripts/components/random.js
+++ scripts/components/random.js
@@ -31,13 +31,13 @@ class Random {
 	static case(cases) {
 		const summary = Array.from(cases).reduce((previous, [, percentage]) => previous + percentage, 0);
 		const random = Random.number(0, summary);
-		let start = 0;
+		let begin = 0;
 		for (const [item, percentage] of cases) {
-			const end = start + percentage;
-			if (start <= random && random < end) {
+			const end = begin + percentage;
+			if (begin <= random && random < end) {
 				return item;
 			}
-			start = end;
+			begin = end;
 		}
 		throw new ReferenceError(`Can't select value. Maybe stack is empty.`);
 	}
