b7b25711c1d65037b9ced276063f6fda11e47d94
Arman <eccs0103@gmail.com>
2023-11-06 18:36:18 +0400

    2.3.2

--- scripts/modules/executors.js
+++ scripts/modules/executors.js
@@ -73,7 +73,37 @@ class Engine extends EventTarget {
 	}
 }
 //#endregion
+//#region Display
+/**
+ * @template {RenderingContext} T
+ */
+class Display extends Engine {
+	/**
+	 * @param {T} context 
+	 * @param {Boolean} launched 
+	 */
+	constructor(context, launched = false) {
+		super(launched);
+		this.#context = context;
+		const canvas = this.#context.canvas;
+		this.addEventListener(`resize`, (event) => {
+			if (canvas instanceof HTMLCanvasElement) {
+				const { width, height } = canvas.getBoundingClientRect();
+				canvas.width = width;
+				canvas.height = height;
+				this.dispatchEvent(new Event(`render`));
+			}
+		});
+		window.addEventListener(`resize`, (event) => {
+			this.dispatchEvent(new Event(`resize`));
+		});
+	}
+	/** @type {RenderingContext} */ #context;
+}
+//#endregion
+
 
 export {
-	Engine
+	Engine,
+	Display
 };
