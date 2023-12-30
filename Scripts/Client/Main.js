98ebf6dd4b4af4069215b1a6a0fdb304a2a8df6e
Arman <eccs0103@gmail.com>
2023-12-30 20:35:09 +0400

    2.3.6

--- Scripts/Client/Main.js
+++ Scripts/Client/Main.js
@@ -2,10 +2,8 @@
 
 import { } from "./Structure.js";
 
-void async function () {
-	try {
-		// Your code goes here
-	} catch (error) {
-		await window.prevent(document.analysis(error));
-	}
-}();
+try {
+	// Your code goes here
+} catch (error) {
+	await window.prevent(document.analysis(error));
+}
