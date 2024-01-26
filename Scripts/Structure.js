8441b78cfd350a5092caae991891c30edb441679
Arman <eccs0103@gmail.com>
2024-01-26 19:52:19 +0400

    2.5.0

--- Scripts/Structure.js
+++ Scripts/Structure.js
@@ -1,7 +1,12 @@
 "use strict";
 
-import { } from "./Modules/Storage.js";
+import { } from "./Modules/Executors.js";
 import { } from "./Modules/Extensions.js";
+import { } from "./Modules/Generators.js";
+import { } from "./Modules/Measures.js";
+import { } from "./Modules/Palette.js";
+import { } from "./Modules/Storage.js";
+import { } from "./Modules/Time.js";
 
 //#region Metadata
 const developer = document.getElement(HTMLMetaElement, `meta[name="author"]`).content;
