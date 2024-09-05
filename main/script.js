d38cbe8fbc2498552cacfd907a1862cbc9846f02
Arman <eccs0103@gmail.com>
2024-09-05 22:18:57 +0400

    3.0.7

--- main/script.js
+++ main/script.js
@@ -8,11 +8,26 @@ import { } from "../scripts/structure.js";
  */
 class Controller {
 	/**
-	 * Contains the main logic for the application.
+	 * Preloads necessary data or resources before running the main logic.
+	 * @returns {Promise<void>}
+	 */
+	async #preload() {
+		// Your preload logic goes here
+	}
+	/**
+	 * Executes the main run logic of the application.
+	 * @returns {Promise<void>}
+	 */
+	async #run() {
+		// Your run logic goes here
+	}
+	/**
+	 * This method orchestrates the preload process and then starts the main application flow.
 	 * @returns {Promise<void>}
 	 */
 	async main() {
-		// Your code goes here
+		await this.#preload();
+		await window.load(this.#run());
 	}
 };
 const controller = new Controller();
