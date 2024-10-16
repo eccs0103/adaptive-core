650dea9509b16cbe97f00ced5b78940a46f78a84
Arman <eccs0103@gmail.com>
2024-10-17 00:14:23 +0400

    3.1.1

--- scripts/structure.js
+++ scripts/structure.js
@@ -3,7 +3,7 @@
 import { } from "./modules/extensions.js";
 import { } from "./modules/generators.js";
 import { } from "./modules/measures.js";
-// import { } from "./modules/palette.js";
+import { } from "./modules/palette.js";
 import { } from "./modules/storage.js";
 
 // Your structure goes here
